dd3dc5e0e1bf1e9b5bb83aa42514f954480daa25
yuvanikaac <156583870+yuvanikaac@users.noreply.github.com>
2025-03-04 21:18:30 +0530

    Add files via upload

---
+++ AddMul.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int d;
+	int a,b,c,e;
+	cout<<"Enter the number of  digits in each value : "<<endl;
+	cin>>d;
+	cout<<"Enter the numbers:"<<endl;
+	cin>>a>>b>>c;
+	cout<<"The sum is"<<a+b+c+e<<endl;
+	
+}
